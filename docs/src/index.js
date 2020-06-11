0dd81b76c6a9076880131c54da535d8661159b75
yk-hts <chyk18074@g.nihon-u.ac.jp>
2020-06-11 12:34:52 +0900

    submit

---
+++ docs/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+
+
+import 'bulma/css/bulma.css'
+
+import '@fortawesome/fontawesome-free/css/brands.css'
+import '@fortawesome/fontawesome-free/css/fontawesome.css'
+import '@fortawesome/fontawesome-free/css/solid.css'
+
+render(<App />, document.querySelector("#content"));
